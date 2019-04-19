5575f700e8dfb3b234e45e82de26211d2ad08703
DevTaeho <kimthzx@gmail.com>
2019-04-19 20:13:46 +0900

    추가 : console.log(response)

--- fetch.js
+++ fetch.js
@@ -1,5 +1,6 @@
 function fetch_print(self){
   fetch(self).then(function(response){
+    console.log(response);
     response.text().then(function(text){
       document.querySelector('article').innerHTML = text;
     })
