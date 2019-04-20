51af76a6395be2dcb0e5e62921d9d242f700a126
DevTaeho <kimthzx@gmail.com>
2019-04-21 00:34:52 +0900

    response 객체 status 활용

--- fetch.js
+++ fetch.js
@@ -1,8 +1,12 @@
 function fetch_print(self){
   fetch(self).then(function(response){
     console.log(response);
-    response.text().then(function(text){
-      document.querySelector('article').innerHTML = text;
-    })
+    if(response.status == '404'){
+      alert('Not found');
+    }else{
+      response.text().then(function(text){
+        document.querySelector('article').innerHTML = text;
+      })
+    }  
   })
 }
