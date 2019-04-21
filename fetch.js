a15f79ac1bc7346920f4aacfadd3dcb10fb8b0d4
DevTaeho <kimthzx@gmail.com>
2019-04-21 14:59:39 +0900

    문법 수정

--- fetch.js
+++ fetch.js
@@ -1,21 +1,19 @@
-var my_Fetch{
-  function fetch_print(self){
-    fetch(self).then(function(response){
-      console.log(response);
-      if(response.status == '404'){
-        alert('Not found');
-      }else{
-        response.text().then(function(text){
-          document.querySelector('article').innerHTML = text;
-        })
-      }
-    })
-  }
+function fetch_print(self){
+  fetch(self).then(function(response){
+    console.log(response);
+    if(response.status == '404'){
+      alert('Not found');
+    }else{
+      response.text().then(function(text){
+        document.querySelector('article').innerHTML  text;
+      })
+    }
+  })
+}
 
-  var _hash = window.location.hash;
-  if(_hash){
-    fetch_print(_hash.substr(2));
-  }else{
-    fetch_print('welcome');
-  }
+var _hash = window.location.hash;
+if(_hash){
+  fetch_print(_hash.substr(2));
+}else{
+  fetch_print('welcome');
 }
